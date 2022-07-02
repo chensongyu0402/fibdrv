077c45b781fdbd07ab6be60fc57efba07e6b4dde
shibainu <kobe29856907@gmail.com>
2022-07-02 21:12:52 +0800

    Add function for doubling method

--- bn.c
+++ bn.c
@@ -1,16 +1,16 @@
 #include "bn.h"
 #include <linux/minmax.h>
 #include <linux/slab.h>
-/*
-bool bn_new(bn_t *bn_ptr,unsigned long long length)
+/* new binaray */
+bool bn_new(bn_t *bn_ptr, unsigned long long length)
 {
     bn_ptr->length = 0;
     if ((bn_ptr->num =
-            kmalloc(sizeof(unsigned long long) * length, GFP_KERNEL)))
+             kmalloc(sizeof(unsigned long long) * length, GFP_KERNEL)))
         bn_ptr->length = length;
     return !!bn_ptr->num;
 }
-*/
+
 /* set all zero */
 bool bn_zrenew(bn_t *bn_ptr, unsigned long long length)
 {
@@ -28,7 +28,6 @@ bool bn_zrenew(bn_t *bn_ptr, unsigned long long length)
     return true;
 }
 /* set specific length to zero*/
-/*
 bool bn_extend(bn_t *bn_ptr, unsigned long long length)
 {
     unsigned long long origin = bn_ptr->length;
@@ -44,7 +43,7 @@ bool bn_extend(bn_t *bn_ptr, unsigned long long length)
     bn_ptr->length = length;
     bn_ptr->num = tmp;
     return true;
-}*/
+}
 /* free memory*/
 void bn_free(bn_t *bn_ptr)
 {
@@ -68,22 +67,21 @@ bool bn_add(const bn_t *a, const bn_t *b, bn_t *res)
     return true;
 }
 /* sub binary number*/
-/*
 bool bn_sub(const bn_t *a, const bn_t *b, bn_t *res)
 {
     if (!bn_zrenew(res, max(a->length, b->length)))
         return false;
 
     if (a->length > b->length) {
-        bn_toggle_move(a, res);// res = -a - 1
-        bn_add_carry(b, res, 0);// res  = -a - 1 + b = b - a - 1
-        bn_toggle_move(res, res); // res = -res = -(b-a-1)-1 = a - b
+        bn_toggle_move(a, res);    // res = -a - 1
+        bn_add_carry(b, res, 0);   // res  = -a - 1 + b = b - a - 1
+        bn_toggle_move(res, res);  // res = -res = -(b-a-1)-1 = a - b
     } else {
-        bn_toggle_move(b, res); // res = -b - 1
-        bn_add_carry(a, res, 1); // res = -b - 1 + a + 1 = a - b
+        bn_toggle_move(b, res);   // res = -b - 1
+        bn_add_carry(a, res, 1);  // res = -b - 1 + a + 1 = a - b
     }
     return true;
-}*/
+}
 /* move a to res(final number) */
 bool bn_move(const bn_t *a, bn_t *res)
 {
@@ -97,7 +95,6 @@ bool bn_move(const bn_t *a, bn_t *res)
     return true;
 }
 /* move 1’s complement of a to res */
-/*
 bool bn_toggle_move(const bn_t *a, bn_t *res)
 {
     if (a->length > res->length)
@@ -109,7 +106,6 @@ bool bn_toggle_move(const bn_t *a, bn_t *res)
         res->num[i] = ~res->num[i];
     return true;
 }
-*/
 /* add and carry */
 void bn_add_carry(const bn_t *b, bn_t *res, int carry)
 {
@@ -128,6 +124,121 @@ void bn_add_carry(const bn_t *b, bn_t *res, int carry)
         msb &= ~res->num[i];
         carry = !!(msb & 1ULL << 63);
     }
+}
+/* left shift */
+bool bn_lshift(bn_t *res, unsigned long long bits)
+{
+    if (!bits)
+        return false;
+    unsigned long long shift_len = (bits >> 3) / sizeof(shift_len) + 1;
+    unsigned long long mod_bits = bits & 0x3fULL;
+    bool remained = true;
+
+    if (shift_len > res->length)
+        remained = false;
+    else
+        for (int i = res->length - shift_len; i < res->length; i++)
+            if (res->num[i] > 0)
+                remained = false;
+
+    if (!remained && !bn_extend(res, shift_len + res->length))
+        return false;
+
+    for (int i = res->length - 1; i >= shift_len; i--)
+        res->num[i] = (res->num[i - shift_len + 1] << mod_bits) |
+                      ((res->num[i - shift_len] >> (63 - shift_len)) >> 1);
+    res->num[shift_len - 1] = res->num[0] << mod_bits;
+    for (int i = 0; i < shift_len - 1; i++)
+        res->num[i] = 0;
+    return true;
+}
+/* right shift */
+void bn_rshift(bn_t *res, unsigned long long bits)
+{
+    if (!bits)
+        return false;
+    unsigned long long shift_len = bits / 64;
+    unsigned long long mod_bits = bits & 0x3fULL;
+
+    if (shift_len > res->length) {
+        memset(res->num, 0, res->length * sizoef(unsigned long long));
+        res->length = 1;
+        return;
+    }
+    int i;
+    for (i = 0; i < res->length - shift_len - 1; i++)
+        res->num[i] = (res->num[i + shift_len] << mod_bits) |
+                      ((res->num[i + shift_len + 1] << (63 - shift_len)) << 1);
+    res->num[res->length - shift_len - 1] =
+        res->num[res->length - 1] >> mod_bits;
+    for (i = res->length - shift_len; i < res->length - 1; i++)
+        res->num[i] = 0;
+    res->length = res->length - shift_len;
+    return true;
+}
+/* mask */
+void bn_mask(bn_t *ptr, unsigned long long msk)
+{
+    for (int i = 0; i < ptr->length; i++)
+        ptr->num[i] &= mask;
+}
+/* shrink */
+bool bn_shrink(bn_t *ptr)
+{
+    int i;
+    for (i = ptr->length - 1; i >= 0; i--) {
+        if (ptr->num[i] > 0)
+            break;
+    }
+    ptr->length = i + 1;
+    if (ptr->length == 0)
+        ptr->length = 1;
+    return true;
+}
+/* multiply */
+bool bn_mult(bn_t *a, bn_t *res)
+{
+    bn_shrink(a);
+    bn_shrink(res);
+    bn_t low = {};
+    if (!bn_znew(&low, res->length))
+        return false;
+    bn_t high = {};
+    if (!bn_znew(&high, a->length + res->length + 5))
+        return false;
+    bn_t sum = {};
+    if (!bn_znew(&sum, a->length + res->length + 5))
+        return false;
+    for (int i = 0; i < a->length; i++) {
+        for (int j = 0; j < 64; j += 32) {
+            unsigned long long multiplier = ((a->num[i]) >> j) & 0xffffffffULL;
+            bn_extend(&low, res->length);
+            bn_move(res, &low);
+            bn_extend(&high, res->length);
+            bn_move(res, &high);
+            bn_rshift(&high, 32);
+            bn_mask(&low, 0xffffffffULL);
+            bn_mask(&high, 0xffffffffULL);
+
+            for (int k = 0; k < low.length; k++)
+                low.num[k] *= multiplier;
+            for (int k = 0; k < high.length; k++)
+                high.num[k] *= multiplier;
+
+            bn_lshift(&high, 32);
+            bn_extend(&hugh, low.length + 1);
+            bn_add_carry(&low, &high, 0);
+            bn_lshift(&high, i * 64 + j);
+            bn_extend(&sum, high.length + 1);
+            bn_add_carry(&high, &sum, 0);
+        }
+    }
+    bn_swap(&sum, res);
+    bn_free(&sum);
+    bn_free(&low);
+    bn_free(&high);
+    bn_shrink(res);
+    return true;
 }
 /* swap a and b*/
 void bn_swap(bn_t *a, bn_t *b)
